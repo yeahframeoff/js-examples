0e210df36a9cbd73d569892b91b261a7df03b2dd
Anton Yefremov <anthyef@gmail.com>
2015-03-31 04:30:43 +0300

    Widget 13 - my own implementation of tabview

---
+++ widgets/13/script.js
@@ -0,0 +1,17 @@
+﻿var tabs;
+var id;
+
+$(function () {
+    tabs = $('.active, .nonactive');
+    tabs.css('width', (100 / tabs.length) + '%');
+    $('.content').hide();
+    $('#tab1.content').show();
+    tabs.click(function () {
+        id = $(this).attr('id');
+        //$('.content').scrollTop(0);
+        $('.content').hide();
+        $(tabs).attr('class', 'nonactive');
+        $(this).attr('class', 'active');
+        $('#' + id + '.content').show();
+    });
+});
